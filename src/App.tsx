fc9d10cdc6463b04d57e8f45fa9ca08698cfe4c4
Joao Macedo <joaovictormacedo91@gmail.com>
2021-12-21 17:19:03 -0300

    Feat: StyledComponents
    Added the styled-components lib to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+
+  button {
+    background-color: black;
+  }
+`;
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>
+        Hello World
+        <button>Salvar</button>
+      </Title>
     </div>
   );
 }
