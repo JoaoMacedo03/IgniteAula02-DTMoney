fb0bde107e71324386982c345af85f114f9db1e1
Joao Macedo <joaovictormacedo91@gmail.com>
2021-12-22 01:25:24 -0300

    feat: Created the hook useTransactions
    It was the created this hook, just to remove the call of
    TransactionContext on every file that was using it. Now you just have to
    call the useTransactions hook, and get the data from it. Only one import
    now.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,14 +1,12 @@
-import { useContext } from 'react';
-
 import incomeImg from '../../assets/income.svg';
 import totalImg from '../../assets/total.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
 import { Container } from "./styles";
-import { TransactionContext } from '../../TransactionContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 export function Summary() {
-  const { transactions } = useContext(TransactionContext);
+  const { transactions } = useTransactions();
 
   const summary = transactions.reduce((acc, transaction) => {
     if(transaction.type === 'deposit') {
